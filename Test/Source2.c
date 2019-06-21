ec3147f1fc7c0f5db8e4e897e5996dc898aeb526
josephinep-digipen <j.passananti@digipen.edu>
2019-06-21 16:11:01 -0700

    another one

---
+++ Test/Source2.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int i;
+
+	printf("Enter your age:  ");
+	scanf("%d", &i);
+	printf("age = %d\n", i);
+	return 0;
+}
